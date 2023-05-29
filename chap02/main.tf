6f5d8a131b0bb41be40edddd73fba06f6172c295
ltsimibilboa <ltsimibiloa@yext.com>
2023-05-29 14:58:38 +0200

    chap02: add user_data and security group for 8080

--- chap02/main.tf
+++ chap02/main.tf
@@ -9,4 +9,23 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+  
+  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
