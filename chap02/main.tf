30b91c1974b202e3aa4a8c9c85a030151da7d6ae
ltsimibilboa <ltsimibiloa@yext.com>
2023-05-29 16:59:43 +0200

    chap2: add autoscaling group and loadbalancer

--- chap02/main.tf
+++ chap02/main.tf
@@ -7,13 +7,13 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
+data "aws_availability_zones" "all" {
+  
+}
 
-  tags = {
-    Name = "terraform-example"
-  }
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-40d28157"
+  instance_type = "t2.micro"
 
   user_data = <<-EOF
             #!/bin/bash
@@ -21,7 +21,11 @@ resource "aws_instance" "example" {
             nohup busybox httpd -f -p "${var.server_port}" &
             EOF
   
-  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+  security_groups = [ "${aws_security_group.instance.id}" ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -33,8 +37,73 @@ resource "aws_security_group" "instance" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = data.aws_availability_zones.all.names
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups = [ "${aws_security_group.elb.id}" ]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
 }
 
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = "${aws_elb.example.dns_name}"
 }
