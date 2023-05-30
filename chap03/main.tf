7d9db87452cfe5b3254bec5d88b21b0e50301710
ltsimibilboa <ltsimibiloa@yext.com>
2023-05-30 17:10:27 +0200

    chap03: create terraform state s3 bucket for remote state

---
+++ chap03/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-lionel"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-lionel"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+    encrypt = true
+  }
+}
