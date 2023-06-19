bef63c2811ec144347e4a682790af5bccdabc94d
bilionel <tblionel15@gmail.com>
2023-06-19 10:00:16 +0200

    chap04: switch back to local module webserver-cluster

--- chap04/stage/services/webserver-cluster/main.tf
+++ chap04/stage/services/webserver-cluster/main.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 module "webserver_cluster" {
-  source = "git@github.com:brikis98/terraform-up-and-running-code.git//code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster?ref=v0.0.2"
+  source = "../../../modules/services/webserver-cluster"
   
   cluster_name = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-lionel"
